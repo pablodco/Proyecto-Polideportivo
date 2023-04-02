61fb18d7da0fc03fce497abb16be0ced820d7f65
Pablo del Campo Olano <pablodel.campo@opendesuto.es>
2023-04-02 21:59:05 +0200

    creacion de tablas todavia incompleta

--- Proyecto Polideportivos/baseDeDatos.c
+++ Proyecto Polideportivos/baseDeDatos.c
@@ -5,4 +5,42 @@
  *      Author: pablo
  */
 
+#include "basededatos.h"
+#include <stdio.h>
+cargarBD(){
+	sqlite3* bd;
+	sqlite3_stmt* stmt;
+	int result=sqlite3_open("PolisBD", &bd);
+	if (result== SQLITE_OK){
+		printf("error al abrir la base de datos");
+	}
+	char* sql="create table Admin if not exists(int id Primary KEY;string nombre,string apellido,string email,string contraseña,int id_p)";
+	result= sqlite3_prepare(bd, sql, strlen(sql)+1, stmt, NULL);
+	if (result==SQLITE_OK){
 
+	}sqlite3_step(stmt);
+	sql="create table Usuario if not exists(int id Primary KEY;string nombre,string apellido,string email,string contraseña,string dir,string ciudad)";
+	result= sqlite3_prepare(bd, sql, strlen(sql)+1, stmt, NULL);
+	if (result==SQLITE_OK){
+
+	}sqlite3_step(stmt);
+	sql="create table Reserva if not exists(int id Primary KEY;string nombre,string apellido,string email,string contraseña)";
+	result= sqlite3_prepare(bd, sql, strlen(sql)+1, stmt, NULL);
+	if (result==SQLITE_OK){
+
+	}sqlite3_step(stmt);
+	sql="create table Instalacion if not exists(int id Primary KEY;string nombre,string deporte)";
+	result= sqlite3_prepare(bd, sql, strlen(sql)+1, stmt, NULL);
+	if (result==SQLITE_OK){
+
+	}sqlite3_step(stmt);
+	sql="create table Poli if not exists(int id Primary KEY;string nombre,string apellido,string email,string contraseña)";
+	result= sqlite3_prepare(bd, sql, strlen(sql)+1, stmt, NULL);
+	if (result==SQLITE_OK){
+
+	}sqlite3_step(stmt);
+
+
+
+
+}
